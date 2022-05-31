a9be914caab0542482a7bf93049639ab723d85e2
shinilsub <deathsizes@hotmail.com>
2022-05-31 17:27:14 +0900

    promise

--- server/controller/tweet.js
+++ server/controller/tweet.js
@@ -1,14 +1,14 @@
 import * as tweetRepository from '../data/tweet.js'
 
-export function getTweets(req, res){
+export async function getTweets(req, res){
     const username = req.query.username;
-    const data = username 
+    const data = await(username 
     ? tweetRepository.getAllByUsername(username)
-    : tweetRepository.getAll();
+    : tweetRepository.getAll());
     res.status(200).json(data);
 }
 
-export function getTweet(req, res){
+export async function getTweet(req, res){
     const id = req.params.id;
     const tweet = tweetRepository.getById(id)
     if(tweet) {
@@ -18,22 +18,22 @@ export function getTweet(req, res){
     }
 }
 
-export function create (req, res) {
+export async function create (req, res) {
     const {text, name, username} = req.body;
-    const tweet = tweetRepository.create(text, name, username);
+    const tweet = await tweetRepository.create(text, name, username);
     
     res.status(201).json(tweet);
 }
-export function upadate(req, res) {
+export async function upadate(req, res) {
     const id = req.params.id;
     const text = req.body.text;
-    const tweet = tweetRepository.update(id, text);
+    const tweet = await tweetRepository.update(id, text);
     if(tweet){
         res.status(200).json(tweet)
     } else {res.status(404).json({message: `Tweet id${id} not found`})}
 }
-export function deleteTweet(req, res) {
+export async function deleteTweet(req, res) {
     const id = req.params.id;
-    tweetRepository.remove(id)
+    await tweetRepository.remove(id)
     res.sendStatus(204)
 }
